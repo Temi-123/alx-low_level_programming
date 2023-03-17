2ca2494de2e2ada229ed27ab53fb634a3a3267f4
Temiloluwa <odarasimi458@gmail.com>
2023-03-17 14:34:31 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	int n:
+	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
-	else if (n<0)
+	n = rand() - RAND_MAX / 2;
+	 if (n<0)
 	{
 		printf("%d is negative/n", n);
 	}
