068ab11bf37492736bbaf7d2a853ef828bc5fb24
Temiloluwa <odarasimi458@gmail.com>
2023-03-17 15:56:10 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 5)
+	if (n > 5)
 	{
 		printf("%d is greater than 5/n", n);
 	}
-	else if (n > 6)
+	else if (n < 6)
 	{
 		printf("%d is less than 6/n", n);
 	}
