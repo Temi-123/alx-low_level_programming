45cc0b0340913eb4d002e62ee5b1b41ad0689629
Temiloluwa <odarasimi458@gmail.com>
2023-03-19 03:21:56 -0700

    the single digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all single digit of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		printf("%d\n", j);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
