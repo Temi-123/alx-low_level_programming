afc3a0115cbfb233af35d169172d5e94aca38370
Temiloluwa <odarasimi458@gmail.com>
2023-03-20 16:30:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@ int main(void)
 { 
           int digit;
 
-          for(digit = 0; digit < 10; digit++)
+ for(digit = 0; digit < 10; digit++)
 	  {
 		  if (digit == 9)
+	{
 		  putchar(digit);
+	{	  
 		  putchar(' ');
 		  putchar(',');
+	}
 	}
 	  putchar('\n');
 return (0);
