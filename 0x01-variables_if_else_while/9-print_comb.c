2142526366ac2768b20d708bd08fa9c3d13e4b5c
Temiloluwa <odarasimi458@gmail.com>
2023-03-20 15:37:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print single digit number;
+ *
+ * Return: return(0)
+ */
+int main(void)
+{ 
+          int digit;
+
+          for(digit = 0; digit < 10; digit++)
+	  {
+		  if (digit == 9)
+		  putchar(digit);
+		  putchar(' ');
+		  putchar(',');
+	}
+	  putchar('\n');
+return (0);
+}
