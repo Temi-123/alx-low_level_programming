fd48ae16ae19e43704f062a4b5c5ba1b1f59dee7
Temiloluwa <odarasimi458@gmail.com>
2023-03-22 15:10:59 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check if argument is alpha
+ *
+ * Return: Return(0)
+ */
+int main(void)
+	int (n)
+{
+	int_isalpha(int c)
+	{
+		if (c >= 97 && c <= 122)
+			return (1);
+		else
+			return (0);
+	}
+'}'
